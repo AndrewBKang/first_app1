fb50f6ba8132e7cb5a0dc661f3ed98973b91b921
Andrew Kang <andrew.b.kang@gmail.com>
2013-03-27 19:18:11 -0400

    Finish static pages

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,30 +5,48 @@ describe "StaticPages" do
   #  describe "GET /static_pages" do
   describe "Home page" do
 
-  	it "should have the content 'First App'" do
+  	it "should have the h1 'First App'" do
   		visit '/static_pages/home'
-  		page.should have_content('First App')
+  		page.should have_selector('h1',:text => 'First App')
   	end
-    # it "works! (now write some real specs)" do
-    #   # Run the generator again with the --webrat flag if you want to use webrat methods/matchers
-    #   get static_pages_index_path
-    #   response.status.should be(200)
-    #  end
+
+  	it "should have the title 'Home'" do
+  		visit '/static_pages/home'
+  		page.should have_selector('title', :text => 
+  			"Ruby on Rails Tutorial First App | Home")
+  	end
+
   end
 
   describe "Help page" do
 
-  	it "should have the content 'Help'" do
+  	it "should have the h1 'Help'" do
   		visit '/static_pages/help'
-  		page.should have_content('Help')
+  		page.should have_selector('h1', :text => 'Help')
   	end
+
+  	it "should have the title 'Help'" do
+  		visit '/static_pages/help'
+  		page.should have_selector('title', :text => 
+  			"Ruby on Rails Tutorial First App | Help")
+  	end
+  
   end
 
   describe "About page" do
+
   	it "should have the content'About Us'" do
   	  visit '/static_pages/about'
   	  page.should have_content('About Us')
   	end
+
+  	it "should have the title 'About Us'" do
+  		visit '/static_pages/about'
+  		page.should have_selector('title', :text => 
+  			"Ruby on Rails Tutorial First App | About Us")
+  	end
+
   end
+
 
 end
